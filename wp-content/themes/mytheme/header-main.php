47c2240bbf05fe8cf11524ce94c08f42f1718237
Christopher Brown <Christopher.Brown@udri.udayton.edu>
2018-06-15 19:15:46 -0400

    Added down arrow to intro screen

--- wp-content/themes/mytheme/header-main.php
+++ wp-content/themes/mytheme/header-main.php
@@ -17,6 +17,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
 	<?php wp_head(); ?>
 </head>
--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -22,13 +22,37 @@ get_header('main');
 		color: white;
 	}
 </style>
-			<div id="primary" class="content-area">
+			<div id="primary intro" class="content-area">
 				<img class = "intro-img" src="<?php echo site_url();?>/wp-content/uploads/2018/06/IMG_0107-e1527997235976.jpg" id="feature_img">
 				<div id = "intro-text">christopher brown</div>
+				<div id="down-arrow">
+					<a href ="#bio-summary" id="down-svg"><i class="fas fa-chevron-circle-down fa-5x"></i></a>
+				</div>
 			</div><!-- #primary -->
+
+			<script>
+				function myMove() {
+					var elem = document.getElementById("down-svg");
+					var up = true;
+					var pos = -1;
+					var id = setInterval(frame, 15);
+					function frame() {
+							if (up && pos > -35) {
+									elem.style.top = pos-- - 50 + 'px';
+							} else if (!up && pos < 0) {
+								elem.style.top = pos++ - 50 + 'px';
+							}
+							else {
+								up = !up;
+							}
+					}
+				}
+				myMove();
+			</script>
 
 			<!-- About me summary -->
 			<section id = "bio-summary">
+				<a name="bio-summary"></a>
 				<div id="bio-text">
 					<h1>who am i?</h1>
 					<ul>
